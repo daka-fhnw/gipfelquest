dd1f04e0496a8cd4cc3a3460052f288b84d4f907
Annik Ege <you@example.com>
2026-05-21 16:04:55 +0200

    Zusammenführen Merkmal 5 mit 2,3,4

--- streamlit/merkmal_2_3_4.py
+++ streamlit/merkmal_2_3_4.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import geopandas as gpd
+import matplotlib.pyplot as plt
 import pandas as pd
 
 alle_berge = pd.read_json("data/gipfel-daten.json").reset_index(drop=True)
@@ -24,12 +26,36 @@ col1, col2, col3 = st.columns(3)
 with col1:
     with st.expander("Merkmal: Schnitt Nord", expanded=True): st.write("")                  #Nordschnitt
 
-    with st.expander("Merkmal: Höhe / Koordinaten"):                                        #Höhe/ Koordinaten
-        st.write(f"Höhe: {st.session_state.richtiger_berg['hoehe'].iloc[0]}")
-        st.write(f"Koordinate Ost: {st.session_state.richtiger_berg['koordinate_x'].iloc[0]}")
-        st.write(f"Koordinate Nord: {st.session_state.richtiger_berg['koordinate_y'].iloc[0]}")
+    with st.expander("Merkmal: Koordinaten / Höhe"):  
 
-    with st.expander("Merkmal: Gebiet / Gletscher / Tal"):                                     #Gebietsnamen/ Kanton
+        #Karte Schweiz mit Punkt
+        schweiz = gpd.read_file("streamlit/LANDESGEBIET.gpkg")
+        punkt_x = st.session_state.richtiger_berg['koordinate_x'].iloc[0]
+        punkt_y = st.session_state.richtiger_berg['koordinate_y'].iloc[0]
+
+        fig, ax = plt.subplots(figsize=(12,12))
+
+        schweiz.plot(
+            ax=ax,
+            facecolor="white",
+            edgecolor="black",
+            linewidth=1)
+        ax.scatter(
+            punkt_x,
+            punkt_y,
+            s=150,
+            color="red")
+
+        ax.axis("off")
+        plt.tight_layout()
+        st.pyplot(fig)  
+
+        st.write(f"Ost: {st.session_state.richtiger_berg['koordinate_x'].iloc[0]}")
+        st.write(f"Nord: {st.session_state.richtiger_berg['koordinate_y'].iloc[0]}")
+        st.write(f"Höhe: {st.session_state.richtiger_berg['hoehe'].iloc[0]}")
+
+    with st.expander("Merkmal: Ort Daten"):
+        st.write(f"Kanton: {st.session_state.richtiger_berg['kanton'].iloc[0]} /                                     #Gebietsnamen/ Kanton")
         st.write(f"Gebiet: {st.session_state.richtiger_berg['gebiet'].iloc[0]}")
         st.write(f"Haupttal: {st.session_state.richtiger_berg['haupttal'].iloc[0]}")
         st.write(f"Gletscher: {st.session_state.richtiger_berg['gletscher'].iloc[0]}")
@@ -40,9 +66,9 @@ with col1:
 with col2:
     with st.expander("Merkmal: Schnitt Ost", expanded=True): st.write("")                   #Ostschnitt
 
-    with st.expander("Merkmal: Orthophoto / Ort"): 
+    with st.expander("Merkmal: Orthophoto / Region"): 
         st.image(st.session_state.richtiger_berg["ortho_url"].iloc[0])  #Orthophoto
-        st.write(f"Kanton: {st.session_state.richtiger_berg['kanton'].iloc[0]} /  Region: {st.session_state.richtiger_berg['grossregion'].iloc[0]}")
+        st.write(f"Region: {st.session_state.richtiger_berg['grossregion'].iloc[0]}")
         
 
 with col3:
