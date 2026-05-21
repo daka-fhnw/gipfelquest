b294e844f72f2558ffe38cded28e0f86c25a29f4
Daniel Käser <daniel.kaeser@students.fhnw.ch>
2026-05-21 13:42:33 +0200

    hoehe aktualisiert

--- merkmale/hoehen.py
+++ merkmale/hoehen.py
@@ -3,19 +3,11 @@ from read_gipfel_koordinaten import read_gipfel_koordinaten
 def get_koordinaten_hoehe_merkmal(data):
     liste = []
     for row in data.itertuples():
-        easting = int(round(row.geometry.x, 0))
-        northing = int(round(row.geometry.y, 0))
         elevation = int(round(row.Hoehe, 0))
-        # spezifischer Code
         liste.append({
-            # z.B. Gebietsname, Profilkoordinaten, ...
-            "easting": easting,
-            "northing": northing,
-            "elevation": elevation
+            "hoehe": elevation
         })
     return liste
-    
-
 
 if __name__ == "__main__":
     data = read_gipfel_koordinaten()
