5b48151beffbb2d7297587e7f82debac0512c8c0
LarsGraenicher <lars2002@bluewin.ch>
2026-05-22 13:19:31 +0200

    Region in Daten eingebaut

--- app/_daten.py
+++ app/_daten.py
@@ -2,13 +2,18 @@ import pandas as pd
 from pandas import DataFrame, Series
 from _konstanten import ANTWORT_OPTIONEN, ANZAHL_GIPFEL
 
+
 def get_gipfel_daten() -> DataFrame:
     gipfel_daten = pd.read_json("data/gipfel-daten.json").reset_index(drop=True)
     gipfel_daten.insert(0, column="id", value=gipfel_daten.index + 1)
     return gipfel_daten
 
-def get_gipfel_auswahl(gipfel_alle: DataFrame) -> DataFrame:
-    return gipfel_alle.sample(n=ANZAHL_GIPFEL).reset_index(drop=True)
+def get_gipfel_auswahl(gipfel_alle: DataFrame, region="alle Berge") -> DataFrame:
+    if region != "alle Berge":
+        gipfel_region = gipfel_region = gipfel_alle[gipfel_alle["region"] == region]
+    else:
+        gipfel_region = gipfel_alle
+    return gipfel_region.sample(n=ANZAHL_GIPFEL).reset_index(drop=True)
 
 def get_antwort_optionen(gipfel_alle: DataFrame, gipfel_zeile: Series) -> DataFrame:
     alle_ohne_gipfel = gipfel_alle[gipfel_alle["id"] != gipfel_zeile["id"]]
