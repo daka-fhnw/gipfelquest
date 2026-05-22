0dbeed9b7c235e647e87cd29f1c69a4fbd5896d7
Daniel Käser <daniel.kaeser@students.fhnw.ch>
2026-05-22 09:11:51 +0200

    Neue Datenbasis

--- merkmale/read_gipfel_koordinaten.py
+++ merkmale/read_gipfel_koordinaten.py
@@ -1,4 +1,5 @@
 import geopandas as gpd
 
 def read_gipfel_koordinaten():
-    return gpd.read_file("./data/gipfel-koordinaten.gpkg")
+    #return gpd.read_file("./data/gipfel-koordinaten.gpkg")
+    return gpd.read_file("./data/bekannte-gipfel-koordinaten.gpkg")
