08847b7da2400f91c915989093f15a28be95c580
Daniel Käser <daniel.kaeser@students.fhnw.ch>
2026-05-22 18:14:51 +0200

    Überschrift Einstellungen

--- app/_einstellungen.py
+++ app/_einstellungen.py
@@ -13,10 +13,14 @@ REGIONEN_OPTIONEN = [
     REGION_TESSIN_GRAUBUENDEN,
 ]
 
-def einstellungen_inhalt(state: AppState):
+def region_einstellung(state: AppState):
     def region_on_change():
         state.einstellungen.region = st.session_state[REGION_KEY]
     auswahl_index = REGIONEN_OPTIONEN.index(state.einstellungen.region)
     st.radio("Mit welchen Bergen möchtest du spielen?", REGIONEN_OPTIONEN, 
              index=auswahl_index, key=REGION_KEY, on_change=region_on_change)
     st.button('Zurück', on_click=state.start_anzeigen)
+
+def einstellungen_inhalt(state: AppState):
+    st.markdown("# Einstellungen")
+    region_einstellung(state)
