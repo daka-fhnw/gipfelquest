786666064b6fa0a85fd775df2a11561deb140f86
Gabriel Blaas <gabriel.blaas@students.fhnw.ch>
2026-05-22 07:13:09 +0200

    Visuelle Verbesserung Grafik Profil

--- streamlit/streamlit_profil.py
+++ streamlit/streamlit_profil.py
@@ -1,9 +1,6 @@
-import json
-import requests
 import streamlit as st
 import matplotlib.pyplot as plt
 from matplotlib.axes import Axes
-from enum import Enum
 import pandas as pd
 
 
@@ -14,6 +11,7 @@ if 'richtiger_berg' not in st.session_state:
     st.session_state['richtiger_berg'] = alle_berge.sample(n=1, random_state=None)
 ####################################################################################
 
+import numpy as np
 
 def plot_profile_data(data: tuple[list[float], list[float]], 
                       ax: Axes) -> None:
@@ -21,13 +19,21 @@ def plot_profile_data(data: tuple[list[float], list[float]],
     ax.set_xticks([])
     ax.set_yticks([])
     x, y = zip(*data)
-    ax.plot(x, y)
+
+    # Feststellung Extremwerte für spätere gleiche Einfärbung
+    y_E = np.array([p[1] for p in data_E])
+    y_N = np.array([p[1] for p in data_N])
+    global_min = min(y_E.min(), y_N.min())
+
+    ax.plot(x,y,color="black",linewidth=1)  # <- Design Graph, vielleicht anpassan an Gesamtdesign
+    ax.fill_between(x,y,global_min,color="lightgray") # <- Hintergrundfarbe Profil, vielleicht anpassan an Gesamtdesign
+    ax.set_facecolor("#D3ECF8") # <- Hintergrundfarbe Himmel, vielleicht anpassan an Gesamtdesign
 
 berg = st.session_state['richtiger_berg'].iloc[0]
 data_E = berg.profil_ost
 data_N = berg.profil_nord
 
-print(data_E)
+#print(data_N)
 
 
 fig = plt.figure()
@@ -35,6 +41,8 @@ gs = fig.add_gridspec(1, 2, wspace=0)
 (ax1, ax2) = gs.subplots(sharey=True)
 plot_profile_data(data_E, ax1)
 plot_profile_data(data_N, ax2)
+ax1.set_title("Profil von West nach Ost")
+ax2.set_title("Profil von Süd nach Nord")
 
 st.pyplot(fig)
 
