d5de0e15bf050e9698c5e880a35d2cdde457d987
LarsGraenicher <lars2002@bluewin.ch>
2026-05-22 11:17:10 +0200

    Bekannte Berge um Region erweitert, Todos abgehackt

--- data/bekannte-gipfel-formatieren.py
+++ data/bekannte-gipfel-formatieren.py
@@ -1,10 +1,11 @@
 import geopandas as gpd
  
-data = gpd.read_file("./quellen/Bekannte_Berggipfel_2.gpkg")
+data = gpd.read_file("./quellen/Bekannte_Berggipfel_2_alle_mit_Region.gpkg")
 
 data["Name"] = data["name"]
 data["Hoehe"] = data["hoehe"].str.replace(" m", "", regex=False).astype(float)
 data["geometry"] = data["geometry"].force_2d()
+data["Region"] = data["region"]
 
-data = data[["Name", "geometry", "Hoehe"]]
+data = data[["Name", "geometry", "Hoehe", "Region" ]]
 data.to_file("data/bekannte-gipfel-koordinaten.gpkg", driver="GPKG")
